c7122ab310e27fe263597ec33c62d668c4bf3306
Felipazo337 <felipe.369.az@gmail.com>
2025-12-01 19:32:29 -0300

    Update def.h

--- TareaSemestral/headers/def.h
+++ TareaSemestral/headers/def.h
@@ -1,3 +1,7 @@
+#ifndef DEF_H
+#define DEF_H
+
+
 #define VACIO 0
 #define MURO 1
 #define IRROMPIBLE 3
@@ -7,5 +11,17 @@
 #define JUGADOR2 7
 #define TP1 8
 #define TP2 9
-#define FILAS 18
-#define COLUMNAS 17
+#define TBLOQUE 1
+#define FILAS 14
+#define COLUMNAS 15
+
+void bordearmapa(int x,int y,int **m,int material);
+
+void random(int *x, int max, int min);
+
+void rellenar(int nx,int ny,int *c,int **m, int material);
+
+
+
+
+#endif
