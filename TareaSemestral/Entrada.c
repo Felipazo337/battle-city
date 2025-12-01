737bb81b51eb23e86c86e3ddb1bb8f84f7085564
Felipazo337 <felipe.369.az@gmail.com>
2025-12-01 18:51:33 -0300

    Update Entrada.c

--- TareaSemestral/Entrada.c
+++ TareaSemestral/Entrada.c
@@ -19,17 +19,17 @@
 #undef random
 #undef rellenar
 
-static void bordearmapa(int x,int y,int **m,int material){
+void bordearmapa(int x,int y,int **m,int material){
     for(int i= 0; i<y; i++){
         for(int j= 0; j<x; j++){
             if((j==0) || (i==0) || (i==y-1) || (j==x-1)) m[i][j]= material;
         }
     }
 }
-static void random(int *x, int max, int min){
+void random(int *x, int max, int min){
     (*x)= (rand()%(max-min+1))+min;
 }
-static void rellenar(int nx,int ny,int *c,int **m, int material){
+void rellenar(int nx,int ny,int *c,int **m, int material){
     if(m[ny][nx]==VACIO) (*c)++;
     m[ny][nx]= material;
 }
